604f70df219ccc7d1cb8e0105309f5c691586ce6
jonmlas <jonmlas@gmail.com>
2025-08-21 19:05:02 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -5,13 +5,27 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Social', 'fw' );
-$manifest['description'] = __( 'Use this extension to configure all your social related APIs. Other extensions will use the Social extension to connect to your social accounts.', 'fw' );
-$manifest['version'] = '1.0.7';
-$manifest['display'] = true;
-$manifest['standalone'] = true;
-$manifest['github_update'] = 'ThemeFuse/Unyson-Social-Extension';
+$manifest['slug']        = 'unysonplus-social';
+$manifest['description'] = __( 
+	'Use this extension to configure all your social related APIs. Other extensions will use the Social extension to connect to your social accounts.', 
+	'fw' 
+);
 
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Social-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/social/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+$manifest['version']     = '1.0.7';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Social-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Social-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
